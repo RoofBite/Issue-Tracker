f8cf80547174f8ea845a87d93f2d6a7fea16eb69
Przemysław Maryńczak <marynczak.przemyslaw@gmail.com>
2021-07-17 17:24:34 +0200

    Issues 'in my projects" shown on my-issues page

--- issue_tracker/models.py
+++ issue_tracker/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Project(models.Model):
     name=models.TextField(null=True)
-    members=models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank= True)
+    member=models.ManyToManyField(User,blank= True)
 
     def __str__(self):
         return self.name
--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from django.db.models import Q
 # Create your views here.
 
 def main(request):
@@ -8,6 +9,12 @@ def main(request):
 def my_issues(request):
     # Later I will add login and will chack if user is logged in and if has access to issue
     if request.method=='GET':
-        issues=Issue.objects.filter(user_assigned=request.user)
-        context={'issues':issues}
+        
+        my_projects=Project.objects.filter(member=request.user)
+        my_project_issues=Issue.objects.filter(project__in=my_projects)
+        
+        my_issues=my_project_issues.filter(user_assigned=request.user)
+        print(my_issues[0].user_assigned)
+        context={'my_issues':my_issues,'my_project_issues':my_project_issues}
         return render(request,'issue_tracker/my_issues.html',context)
+
