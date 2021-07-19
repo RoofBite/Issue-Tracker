95b29d0baf4641e2a6f30f7b6f4f89b0ce49f9a4
Przemysław Maryńczak <marynczak.przemyslaw@gmail.com>
2021-07-19 16:52:18 +0200

    Two paginators to my_issues added

--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -5,7 +5,7 @@ from django.db.models import Q
 from .models import *
 from .forms import IssueForm, IssueTagForm
 from django.views.generic import CreateView
-# Create your views here.
+from django.core.paginator import Paginator, EmptyPage
 
 def main(request):
     return render(request,'issue_tracker/index.html')
@@ -23,34 +23,34 @@ class Add_issue(CreateView):
         kwargs['request'] = self.request
         return kwargs
 
-'''
-def add_issue(request):
-    
-    if request.method == "POST":
-        form = super().get_form(*args, **kwargs)  # Get the form as usual
-        user = self.request.user
-        form.fileds['projct'].queryset = Project.objects.filter(member=user)
 
-        form = IssueForm(request.POST)
-        if form.is_valid():
-            form.save()
-            
-            return redirect('issue_tracker:main')
-    else:    
-        # Initialize empty form
-        form = IssueForm()
-    context={'form':form}
-    return render(request,'issue_tracker/add_issue.html',context)
-'''
 def my_issues(request):
     # Later I will add login and will chack if user is logged in and if has access to issue
     if request.method=='GET':
         
         my_projects=Project.objects.filter(member=request.user)
         my_project_issues=Issue.objects.filter(project__in=my_projects).order_by('-create_date')
-        
         my_issues=my_project_issues.filter(user_assigned=request.user)
         
-        context={'my_issues':my_issues,'my_project_issues':my_project_issues}
+        paginator1 = Paginator(my_issues, 4)
+        paginator2 = Paginator(my_project_issues, 4)
+        
+
+        page_number1 = request.GET.get('page1')
+        try:
+            page_obj1 = paginator1.get_page(page_number1)
+        except EmptyPage:
+            page_obj1=paginator1.page(paginator1.num_pages)
+
+        page_number2 = request.GET.get('page2')
+
+        try:
+            page_obj2 = paginator2.get_page(page_number2)
+        except EmptyPage:
+            page_obj2=paginator2.page(paginator2.num_pages)
+        
+        
+        page_obj2 = paginator2.get_page(page_number2)
+        context={'page_obj1':page_obj1,'page_obj2':page_obj2,'my_issues':my_issues,'my_project_issues':my_project_issues}
         return render(request,'issue_tracker/my_issues.html',context)
 
