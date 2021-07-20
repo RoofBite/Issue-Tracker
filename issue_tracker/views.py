fad34e4aafe7bb177bded7688beb60757df90a70
Przemysław Maryńczak <marynczak.przemyslaw@gmail.com>
2021-07-20 14:09:42 +0200

    Query optimization in my_issues view

--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -27,9 +27,18 @@ class Add_issue(CreateView):
 @require_http_methods(["GET"])
 def my_issues(request):
     # Later I will add login and will chack if user is logged in and if has access to issue 
-    my_projects=Project.objects.filter(member=request.user)
-    my_project_issues=Issue.objects.filter(project__in=my_projects).order_by('-create_date') 
+    #my_projects=Project.objects.filter(member=request.user)
+    #my_project_issues=Issue.objects.filter(project__in=my_projects).order_by('-create_date') 
+    #my_issues=my_project_issues.filter(user_assigned=request.user)
+    
+    my_project_issues = Issue.objects.filter(project__member=request.user).order_by("-create_date").select_related('project','user_assigned')
     my_issues=my_project_issues.filter(user_assigned=request.user)
+    
+    
+    #my_project_issues2 = Issue.objects.filter(project__member=request.user, user_assigned=request.user).order_by("-create_date").select_related('project')
+    #my_project_issues2 = Issue.objects.filter(project__member=request.user, user_assigned=request.user).order_by("-create_date").select_related('project')
+    #print(my_project_issues2)
+
     
     paginator1 = Paginator(my_issues, 4)
     paginator2 = Paginator(my_project_issues, 4)
@@ -51,6 +60,7 @@ def my_issues(request):
     page_obj2 = paginator2.get_page(page_number2)
        
     if request.GET.get('search_query1'):
+        print('search_query1')
         search_query1=request.GET.get('search_query1')
         query1=my_project_issues.filter(
         Q(project__name__icontains=search_query1) | Q(create_date__startswith=search_query1) | Q(update_date__startswith=search_query1) | Q(title__icontains=search_query1) | Q(description__icontains=search_query1) | Q(user_assigned__username__icontains=search_query1)
@@ -65,6 +75,7 @@ def my_issues(request):
         page_obj1=paginator1.page(paginator1.num_pages)
 
     if request.GET.get('search_query2'):
+        print('search_query2')
         search_query2=request.GET.get('search_query2')
         query2=my_issues.filter(
         Q(project__name__icontains=search_query2) | Q(create_date__startswith=search_query2) | Q(update_date__startswith=search_query2) | Q(title__icontains=search_query2) | Q(description__icontains=search_query2) | Q(user_assigned__username__icontains=search_query2)
@@ -77,8 +88,8 @@ def my_issues(request):
         page_obj2 = paginator2.get_page(page_number2)
     except EmptyPage:
         page_obj2=paginator2.page(paginator2.num_pages)
-        
-    #print(page_obj1,page_obj2,my_issues,my_project_issues)
+    
+    #print(my_issues,my_project_issues)
     context={'page_obj1':page_obj1,'page_obj2':page_obj2,
     'my_issues':my_issues,'my_project_issues':my_project_issues}
     return render(request,'issue_tracker/my_issues.html',context)
