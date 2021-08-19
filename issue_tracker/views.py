8490bc3daa4f7086298fc0935269a637fc1b5897
Przemysław Maryńczak <marynczak.przemyslaw@gmail.com>
2021-08-19 16:08:27 +0200

    More adjustments for user in admin group

--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -577,12 +577,18 @@ def manage_project_issues_list(request, pk):
 
 
 @login_required(login_url="issue_tracker:sign-in")
-@group_required("leader", "developer")
+@group_required("leader", "developer", "admin")
 @require_http_methods(["GET"])
 def project_details_old_issues(request, pk):
-    project_instance = Project.objects.filter(
-        Q(id=pk), Q(leader__id=request.user.id) | Q(developer__id=request.user.id)
-    ).first()
+
+    if request.user.groups.filter(name__in=("admin",)):
+        project_instance = Project.objects.filter(id=pk).first()
+
+    elif request.user.groups.filter(name__in=("developer", "leader")):
+        project_instance = Project.objects.filter(
+            Q(id=pk), Q(leader__id=request.user.id) | Q(developer__id=request.user.id)
+        ).first()
+
     if project_instance:
         context = {}
 
@@ -635,12 +641,18 @@ def project_details_old_issues(request, pk):
 
 
 @login_required(login_url="issue_tracker:sign-in")
-@group_required("leader", "developer")
+@group_required("leader", "developer", "admin")
 @require_http_methods(["GET"])
 def project_details(request, pk):
-    project_instance = Project.objects.filter(
-        Q(id=pk), Q(leader__id=request.user.id) | Q(developer__id=request.user.id)
-    ).first()
+
+    if request.user.groups.filter(name__in=("admin",)):
+        project_instance = Project.objects.filter(id=pk).first()
+
+    elif request.user.groups.filter(name__in=("developer", "leader")):
+        project_instance = Project.objects.filter(
+            Q(id=pk), Q(leader__id=request.user.id) | Q(developer__id=request.user.id)
+        ).first()
+
     if project_instance:
         context = {}
 
@@ -705,26 +717,42 @@ def project_details(request, pk):
 
 
 @login_required(login_url="issue_tracker:sign-in")
-@group_required("leader", "developer")
+@group_required("leader", "developer", "admin")
 @require_http_methods(["GET"])
 def issue_details(request, pk):
-    projects = Project.objects.filter(
-        Q(leader__id=request.user.id) | Q(developer__id=request.user.id)
-    )
+
+    if request.user.groups.filter(name__in=("admin",)):
+        projects = Project.objects.all()
+
+    elif request.user.groups.filter(name__in=("developer", "leader")):
+        projects = Project.objects.filter(
+            Q(leader__id=request.user.id) | Q(developer__id=request.user.id)
+        )
+
     issue_instance = Issue.objects.filter(id=pk, project__in=projects).first()
 
     if issue_instance:
         context = {}
-
-        issues = (
-            Issue.history.filter(
-                Q(id=pk),
-                Q(project__leader__id=request.user.id)
-                | Q(project__developer__id=request.user.id),
+        
+        if request.user.groups.filter(name__in=("admin",)):
+            issues = (
+                Issue.history.filter(id=pk)
+                .order_by("-update_date")
+                .select_related("project", "user_assigned")
             )
-            .order_by("-update_date")
-            .select_related("project", "user_assigned")
-        )
+            
+
+        elif request.user.groups.filter(name__in=("developer", "leader")):
+            issues = (
+                Issue.history.filter(
+                    Q(id=pk),
+                    Q(project__leader__id=request.user.id)
+                    | Q(project__developer__id=request.user.id),
+                )
+                .order_by("-update_date")
+                .select_related("project", "user_assigned")
+            )
+
         paginator = Paginator(issues, 3)
         page_number = request.GET.get("page")
 
