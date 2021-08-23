5a73f23e124497f0cd82d0342d13abefbc184dfb
Przemysław Maryńczak <marynczak.przemyslaw@gmail.com>
2021-08-23 12:52:22 +0200

    User can resign from leader position in project

--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -61,6 +61,8 @@ urlpatterns = [
     path("developer-application-deny/<int:pk>/", views.developer_application_deny, name="developer-application-deny"),
     path("project-developer-resign/<int:pk>/", views.project_developer_resign, name="project-developer-resign"),
     path("project-developer-resign-confirm/<int:pk>/", views.project_developer_resign_confirm, name="project-developer-resign-confirm"),
+    path("project-leader-resign/<int:pk>/", views.project_leader_resign, name="project-leader-resign"),
+    path("project-leader-resign-confirm/<int:pk>/", views.project_leader_resign_confirm, name="project-leader-resign-confirm"),
 
     path("manage-leaders-applications-list/", views.manage_leaders_applications_list, name="manage-leaders-applications-list"),
     path("leader-application-accept/<int:pk>/", views.leader_application_accept, name="leader-application-accept"),
--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -789,26 +789,73 @@ def project_details_old_issues(request, pk):
 @group_required("developer")
 @require_http_methods(["GET"])
 def project_developer_resign(request, pk):
-    project = Project.objects.filter(pk=pk).first()
-    context = {"project": project}
-    return render(request, "issue_tracker/project_developer_resign.html", context)
+    project = Project.objects.filter(pk=pk, developer=request.user).first()
+    if project:
+        context = {"project": project}
+        return render(request, "issue_tracker/project_developer_resign.html", context)
+    else:
+        return HttpResponse("You are not allowed to see this page")
 
 
 @login_required(login_url="issue_tracker:sign-in")
 @group_required("developer")
 @require_http_methods(["GET"])
 def project_developer_resign_confirm(request, pk):
-    project = Project.objects.filter(pk=pk).first()
+    project = Project.objects.filter(pk=pk, developer=request.user).first()
     user = request.user
-    project.developer.remove(user)
+    if project:
+        project.developer.remove(user)
 
-    developer_group = Group.objects.get(name="developer")
+        developer_group = Group.objects.get(name="developer")
 
-    # User is not developer anymore, will be deleted from group
-    if not user.project_set.all():
-        developer_group.user_set.remove(user)
+        # User is not developer anymore, will be deleted from group
+        if not user.project_set.all():
+            developer_group.user_set.remove(user)
+            return redirect("issue_tracker:main")
+        return redirect("issue_tracker:my-projects")
+    else:
+        return HttpResponse("You are not allowed to see this page")
 
-    return redirect("issue_tracker:my-projects")
+
+
+
+@login_required(login_url="issue_tracker:sign-in")
+@group_required("leader")
+@require_http_methods(["GET"])
+def project_leader_resign(request, pk):
+    project = Project.objects.filter(pk=pk, leader=request.user).first()
+    if project:
+        context = {"project": project}
+        return render(request, "issue_tracker/project_leader_resign.html", context)
+    else:
+        return HttpResponse("You are not allowed to see this page")
+
+
+@login_required(login_url="issue_tracker:sign-in")
+@group_required("leader")
+@require_http_methods(["GET"])
+def project_leader_resign_confirm(request, pk):
+    user = request.user
+    project = Project.objects.filter(pk=pk, leader=user).first()
+    
+    if project:
+        Project.objects.filter(pk=pk, leader=user).update(leader=None)
+        leader_group = Group.objects.get(name="leader")
+
+        # User is not leader anymore, will be deleted from group
+        if not user.leader_project_set.all():
+            leader_group.user_set.remove(user)
+            return redirect("issue_tracker:main")
+        return redirect("issue_tracker:my-projects")
+    else:
+        return HttpResponse("You are not allowed to see this page")
+
+
+
+
+
+
+
 
 
 @login_required(login_url="issue_tracker:sign-in")
