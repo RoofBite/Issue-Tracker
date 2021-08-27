8bb579a159c44162c34da0b552260b30ca00b110
Przemysław Maryńczak <marynczak.przemyslaw@gmail.com>
2021-08-27 15:12:32 +0200

    project_developer_resign tests added

--- issue_tracker/tests/test_views.py
+++ issue_tracker/tests/test_views.py
@@ -11,7 +11,7 @@ from lazysignup.models import LazyUser
 from lazysignup.utils import is_lazy_user
 from django.db.models import Q
 
-"""
+
 class TestViews_set_demo_user(TestCase):
     def setUp(self):
         self.superuser = User.objects.create_superuser(
@@ -1361,7 +1361,7 @@ class TestView_manage_project_developers(TestCase):
 
         self.assertEquals(response.status_code, 200)
 
-"""
+
 
 class TestView_manage_project_issues_list(TestCase):
     def setUp(self):
@@ -1477,3 +1477,43 @@ class TestView_project_details_old_issues(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertContains(response, "You are not allowed to see this project")
         
+
+class TestView_project_developer_resign(TestCase):
+    def setUp(self):
+        self.superuser = User.objects.create_superuser(
+            "Superuser", "Superuser@example.com", "Password"
+        )
+        self.user = User.objects.create_user("User", "User@example.com", "Password")
+        self.client = Client()
+
+        Group.objects.get_or_create(name="admin")
+        Group.objects.get_or_create(name="developer")
+        Group.objects.get_or_create(name="leader")
+
+        self.project = Project.objects.create(
+            name="Project1", description="Description1", leader=self.user
+        )
+        self.project.developer.add(self.user)
+        self.project2 = Project.objects.create(
+            name="Project2", description="Description2", leader=self.superuser
+        )
+
+    def test_project_developer_resign_developer_group_user_GET_not_allowed(self):
+        group = Group.objects.get(name="developer")
+        group.user_set.add(self.user)
+
+        self.client.force_login(user=self.user, backend=None)
+        response = self.client.get(reverse("issue_tracker:project-developer-resign", args=["2"]), {'search_query': 'Project'})
+
+        self.assertEquals(response.status_code, 200)
+        self.assertContains(response, "You are not allowed to see this page")
+    
+    def test_project_developer_resign_developer_group_user_GET_allowed(self):
+        group = Group.objects.get(name="developer")
+        group.user_set.add(self.user)
+
+        self.client.force_login(user=self.user, backend=None)
+        response = self.client.get(reverse("issue_tracker:project-developer-resign", args=["1"]), {'search_query': 'Project'})
+
+        self.assertEquals(response.status_code, 200)
+
