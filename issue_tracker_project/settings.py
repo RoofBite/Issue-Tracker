02c456ee4f828ea69c6f6ede086a89d4b8a4e9d6
Przemysław Maryńczak <marynczak.przemyslaw@gmail.com>
2021-08-31 18:05:37 +0200

    More preparation for heroku deployment

--- issue_tracker_project/settings.py
+++ issue_tracker_project/settings.py
@@ -12,20 +12,20 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-#from .secrets import secret_key, email, password, datebase_password, datebase_user, datebase_host
+from .secrets import secret_key, email, password, datebase_password, datebase_user, datebase_host
 
-secret_key = os.environ.get('1')
-email = os.environ.get('2')
-password1 = os.environ.get('3')
-password2 = os.environ.get('4')
-password3 = os.environ.get('5')
-password4 = os.environ.get('6')
+secret_key = os.environ.get('a1')
+email = os.environ.get('a2')
+password1 = os.environ.get('a3')
+password2 = os.environ.get('a4')
+password3 = os.environ.get('a5')
+password4 = os.environ.get('a6')
 
 password = str(password1)+str(password2)+str(password3)+ str(password4)
 
-datebase_password = os.environ.get('7')
-datebase_user = os.environ.get('8')
-datebase_host = os.environ.get('9')
+datebase_password = os.environ.get('a7')
+datebase_user = os.environ.get('a8')
+datebase_host = os.environ.get('a9')
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
